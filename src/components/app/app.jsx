8c61db7c228945d90604a85f4af7ff76a4323e12
Vladislav <res.19@yandex.ru>
2020-10-08 16:43:25 +0300

    Добавляет новые компоненты, добавляет маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import WelcomePage from "../welcome-page/welcome-page";
+import ArtistQuestionPage from "../artist-question-page/artist-question-page";
+import GenreQuestionPage from "../genre-question-page/genre-question-page";
+import AuthPage from "../auth-page/auth-page";
+import WinPage from "../win-page/win-page";
+import LosePage from "../lose-page/lose-page";
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
+
   const {errorsCount} = props;
 
   return (
-    <WelcomePage errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomePage errorsCount={errorsCount} />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionPage />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionPage />
+        </Route>
+        <Route exact path="/login">
+          <AuthPage />
+        </Route>
+        <Route exact path="/result">
+          <WinPage />
+        </Route>
+        <Route exact path="/lose">
+          <LosePage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
