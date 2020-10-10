dfb636217183416ac98c88c05377afdae92b92f9
Vladislav <res.19@yandex.ru>
2020-10-11 01:14:48 +0300

    Добавляет рефактор компоненту GenreQuestionPage, добавляет стейт

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -2,19 +2,19 @@ const AVATAR_URL = `https://picsum.photos/200/300?random`;
 export default [
   {
     type: `genre`,
-    genre: `rock`,
+    genre: `disco`,
     answers: [{
-      src: `https://upload.wikimedia.org/wikipedia/commons/c/cc/Handel_-_messiah_-_38_how_beautiful_are_the_feet.ogg`,
-      genre: `classic`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/46/Example_of_double_tracking_in_a_pop-rock_song_%283_guitar_tracks%29.ogg`,
+      genre: `rock`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/e/e3/Andr%C3%A9_Michelle_-_Construct_Infinity.ogg`,
-      genre: `techno`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/0/0e/Stringed_Disco_%28MacLeod%2C_Kevin%29_%28ISRC_USUAN1100059%29.oga`,
+      genre: `disco`,
     }, {
       src: `https://upload.wikimedia.org/wikipedia/commons/b/b2/Original_Dixieland_Jass_Band_-_Livery_Stable_Blues_%281917%29_alternate_edit.ogg`,
       genre: `jazz`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/46/Example_of_double_tracking_in_a_pop-rock_song_%283_guitar_tracks%29.ogg`,
-      genre: `rock`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Tentacles_-_03_-_Disco_Time.ogg`,
+      genre: `disco`,
     }]
   }, {
     type: `artist`,
@@ -27,7 +27,7 @@ export default [
       artist: `Rick and Morty`,
     }, {
       picture: `${AVATAR_URL}=${Math.random()}`,
-      artist: `Johann Bach`,
+      artist: `Felix Mendelssohn`,
     }, {
       picture: `${AVATAR_URL}=${Math.random()}`,
       artist: `Johannes Brahms`,
