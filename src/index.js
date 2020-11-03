5b387a5c42acbcea91c6fe03bbd1dd7ae1098e90
Vladislav <res.19@yandex.ru>
2020-11-03 11:20:19 +0300

    Подключит пакет edux-thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
 import App from "./components/app/app";
 import {reducer} from "./store/reducer";
+import thunk from "redux-thunk";
+import {createAPI} from "./services/api";
+import {composeWithDevTools} from "redux-devtools-extension";
 
+const api = createAPI(
+    () => store.dispatch()
+);
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
 
 ReactDOM.render(
